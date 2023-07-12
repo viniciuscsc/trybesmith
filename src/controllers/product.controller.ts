32a42ef9776e97379441cb2f41365e20d1fa55a2
cunhavcs <cunha.vcs@gmail.com>
2023-07-12 12:34:14 -0300

    Req 01 - cria o endpoint para cadastro de produtos (sem testes)

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import productService from '../services/product.service';
+
+const registerProduct = async (req: Request, res: Response): Promise<Response> => {
+  const productData = req.body;
+
+  const { statusCode, data } = await productService.registerProduct(productData);
+
+  return res.status(statusCode).json(data);
+};
+
+export default {
+  registerProduct,
+};
